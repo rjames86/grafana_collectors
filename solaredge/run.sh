1bd92366ad34f757b8c5bc43ebcd2f9b975907fb
Ryan M <ryan@ryanmo.co>
2023-11-17 09:02:10 -0700

    dont be verbose in solar

--- solaredge/run.sh
+++ solaredge/run.sh
@@ -4,7 +4,7 @@ DELAY=600 # 10 minutes
 
 while (true); do
     echo "$(date) Fetching SolarEdge data"
-    python3 main.py -v
+    python3 main.py
     echo "$(date) sleeping $DELAY seconds"
     sleep $DELAY
 done
