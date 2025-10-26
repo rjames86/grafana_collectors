9c5c233d35291e332f2e6736a91332fe5d96c10e
Ryan M <ryan@ryanmo.co>
2025-10-26 17:16:49 -0600

    update api to write to both dbs

--- api/main.py
+++ api/main.py
@@ -1,24 +1,37 @@
 from flask import Flask, request, jsonify
-from influxdb import InfluxDBClient
+from influxdb import InfluxDBClient as InfluxDBV1Client
+from influxdb_client import InfluxDBClient as InfluxDBV2Client, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 import sys
 from datetime import datetime
 import pytz
 import requests
 from os import environ
+import logging
 
 from env import (
-    INFLUXDB_URL,
-    INFLUXDB_USER,
-    INFLUXDB_PASS,
+    INFLUXDB_V1_URL,
+    INFLUXDB_V1_USER,
+    INFLUXDB_V1_PASS,
+    INFLUXDB_V2_URL,
+    INFLUXDB_V2_TOKEN,
+    INFLUXDB_V2_ORG,
+    PUSHOVER_USER,
+    PUSHOVER_SPRINKLER_TOKEN,
 )
 
 
-influx_client = InfluxDBClient(
-    host=INFLUXDB_URL, username=INFLUXDB_USER, password=INFLUXDB_PASS
+influxV1_client = InfluxDBV1Client(
+    host=INFLUXDB_V1_URL, username=INFLUXDB_V1_USER, password=INFLUXDB_V1_PASS
 )
+influxV2_client = InfluxDBV2Client(url=INFLUXDB_V2_URL, token=INFLUXDB_V2_TOKEN, org=INFLUXDB_V2_ORG)
+write_api = influxV2_client.write_api(write_options=SYNCHRONOUS)
+
 
 app = Flask(__name__)
 
+logger = logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route("/health", methods=["GET"])
 def health_check():
@@ -33,11 +46,37 @@ def write_influxdb_post(database):
 
     if verbose:
         print(data_points)
+
+    try:
+        logger.info("Attempting to write to InfluxDB v2")
+        write_influxdb_v2(database, data_points)
+    except Exception as e:
+        logger.error(f"Failed to write to InfluxDB v2: {e}")
+        pass
+
     try:
         write_influxdb(database, data_points)
         return jsonify(dict(success=True, message="Successfully written"))
     except Exception as e:
         return jsonify(dict(success=False, message=str(e)))
+
+
+def write_influxdb_v2(bucket, data_points):
+    points = []
+    for dp in data_points:
+        p = Point(dp["measurement"])
+        for k, v in dp.get("tags", {}).items():
+            p.tag(k, v)
+        for k, v in dp.get("fields", {}).items():
+            p.field(k, v)
+        if "time" in dp:
+            p.time(dp["time"])
+        points.append(p)
+
+    logger.info(f"Org: {INFLUXDB_V2_ORG}, Bucket: {bucket}")
+
+    write_api.write(bucket=bucket, org=INFLUXDB_V2_ORG, record=points)
+
 
 
 @app.route("/influx/latest_data", methods=["GET"])
@@ -47,15 +86,15 @@ def get_curent_data():
     start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
     start_of_day_ms = int(start_of_day.timestamp() * 1000)
 
-    influx_client.switch_database("solar_edge")
+    influxV1_client.switch_database("solar_edge")
     latest_power_query = f'SELECT last("value") FROM "sensor__power_production" WHERE time >= {start_of_day_ms}ms and time <= now()'
     total_energy_query = f'SELECT sum("value") FROM "sensor__energy_production" WHERE time >= {start_of_day_ms}ms and time <= now()'
     total_energy_consumption_query = f'SELECT sum("value") FROM "sensor__energy_consumption" WHERE time >= {start_of_day_ms}ms and time <= now()'
 
-    latest_power_results = list(influx_client.query(latest_power_query).get_points())[0]
-    total_energy_results = list(influx_client.query(total_energy_query).get_points())[0]
+    latest_power_results = list(influxV1_client.query(latest_power_query).get_points())[0]
+    total_energy_results = list(influxV1_client.query(total_energy_query).get_points())[0]
     total_energy_consumption_results = list(
-        influx_client.query(total_energy_consumption_query).get_points()
+        influxV1_client.query(total_energy_consumption_query).get_points()
     )[0]
 
     last_update = datetime.fromisoformat(latest_power_results["time"])
@@ -85,8 +124,8 @@ def send_pushover_message():
     message = data.get("message", "")
     title = data.get("title", "Alert")
 
-    user_token = environ.get("PUSHOVER_USER")
-    app_token = environ.get("PUSHOVER_SPRINKLER_TOKEN")
+    user_token = PUSHOVER_USER
+    app_token = PUSHOVER_SPRINKLER_TOKEN
 
     resp = requests.post(
         "https://api.pushover.net/1/messages.json",
@@ -99,5 +138,5 @@ def send_pushover_message():
 
 
 def write_influxdb(database, data_points):
-    influx_client.create_database(database)
-    influx_client.write_points(data_points, database=database, batch_size=10000)
+    influxV1_client.create_database(database)
+    influxV1_client.write_points(data_points, database=database, batch_size=10000)
