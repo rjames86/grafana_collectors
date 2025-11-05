260a4fcd384e3dbdd08afbdfeafafd163e736fef
Ryan M <ryan@ryanmo.co>
2025-11-05 14:21:25 -0700

    remove verbose from solaredge

--- solaredge/run.sh
+++ solaredge/run.sh
@@ -4,7 +4,7 @@ DELAY=600 # 10 minutes
 
 while (true); do
     echo "$(date) Fetching SolarEdge data"
-    python3 main.py --verbose
+    python3 main.py
     echo "$(date) sleeping $DELAY seconds"
     sleep $DELAY
 done
